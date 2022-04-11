1ed83c0d574e7160ac432df72a0c542bf0a79962
Your Name <you@example.com>
2022-04-11 18:50:49 +0300

    primary configuration ongoing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 import Orders from './components/Orders/Orders';
 import Shop from './components/Shop/Shop';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/orders' element={<Orders></Orders>}></Route>
         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element = {<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1RzGKUh-_TAHL3lYyd9JpRyf5yF0jZvo",
+  authDomain: "ema-john-115c8.firebaseapp.com",
+  projectId: "ema-john-115c8",
+  storageBucket: "ema-john-115c8.appspot.com",
+  messagingSenderId: "507184411881",
+  appId: "1:507184411881:web:7edfd3ecf1a1abb8662c78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
